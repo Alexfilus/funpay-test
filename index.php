f010eb868fa9a3d6eeb0f444d3e5962940d99ae8
Александр <alexfilus@gmail.com>
2019-04-21 19:56:41 +0300

    обработка исключений

--- index.php
+++ index.php
@@ -5,5 +5,10 @@ $sms = "Пароль: 4737<br />
 Спишется 1005,03р.<br />
 Перевод на счет 410011695752777";
 
-$result = (new \Af\YandexMoney\ParserSMS($sms))->parse();
-
+try {
+    $result = (new \Af\YandexMoney\ParserSMS($sms))->parse();
+}
+catch (\Throwable $exception)
+{
+    echo $exception->getMessage();
+}
